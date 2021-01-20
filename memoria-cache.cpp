5b57093cd189d3636815ca009027cee85e4047d2
DarkusPhantom <luisfrodriguezb.lf@gmail.com>
2021-01-20 16:35:33 -0400

    Actualizacion

--- memoria-cache.cpp
+++ memoria-cache.cpp
@@ -27,9 +27,7 @@ void readConfigIn(int *block_size, bool *emplazamiento , int *block_line_size, b
 	}
 }
 
-//void readTrazIn(queue<int, list<int>> direction_list) {}
-
-void direct_cache_process(list<int> direction_list, int capacity_cache) {
+void process_LRU(list<int> direction_list, int capacity_cache) {
     list<int>::iterator index_direction, indiceAux;
     list<int> cache_list, list_helper;
     int index_bits = log10(capacity_cache) / log10(2);
@@ -39,7 +37,7 @@ void direct_cache_process(list<int> direction_list, int capacity_cache) {
     ofstream resu("resu.out");
 
     cout << "Abriendo archivo...." << endl;
-    cout << "Generando la correspondencia directa..." << endl;
+    cout << "Generando resultados..." << endl;
     resu << "\n" << setw(20) << "Direccion de memoria" << setw(20) << "Hit o Fallo" << setw(30) << "Contenido de los bloques" << endl << setw(65) << "de cache actual" << endl;
     for (index_direction = direction_list.begin(); index_direction != direction_list.end(); index_direction++) {
         bitset<BITS_NUMBER> binary = * index_direction;
@@ -78,6 +76,55 @@ void direct_cache_process(list<int> direction_list, int capacity_cache) {
     cout << "Revise el archivo resu.out" << endl;
 }
 
+// void process_FIFO(list<int> direction_list, int capacity_cache) {
+//     list<int>::iterator index_direction, indiceAux;
+//     list<int> cache_list, list_helper;
+//     int index_bits = log10(capacity_cache) / log10(2);
+//     int bits_tags = BITS_NUMBER - index_bits;
+//     int accerts = 0, fails = 0;
+//     bool hit;
+//     ofstream resu("resu.out");
+
+//     cout << "Abriendo archivo...." << endl;
+//     cout << "Generando resultados..." << endl;
+//     resu << "\n" << setw(20) << "Direccion de memoria" << setw(20) << "Hit o Fallo" << setw(30) << "Contenido de los bloques" << endl << setw(65) << "de cache actual" << endl;
+//     for (index_direction = direction_list.begin(); index_direction != direction_list.end(); index_direction++) {
+//         bitset<BITS_NUMBER> binary = * index_direction;
+//         hit = false;
+
+//         for (indiceAux = list_helper.begin(); indiceAux != list_helper.end() && !hit; indiceAux++) {
+//             if(* index_direction == * indiceAux) hit = true;
+//         }
+
+//         string tag = binary.to_string().substr(0, bits_tags);
+//         string index = binary.to_string().substr(bits_tags);
+//         string direction = tag + " " + index;
+
+//         if (!hit) {
+//             if (list_helper.size() < capacity_cache) {
+//                 cache_list.push_back(* index_direction);
+
+//             } else {
+//                 indiceAux = find(cache_list.begin(), cache_list.end(), list_helper.front());
+//                 (* indiceAux) = (* index_direction);
+//                 list_helper.pop_front();
+//             }
+//             list_helper.push_back(* index_direction);
+//             fails++;
+//             resu << "\n" << setw(20) << direction << setw(20) << "Fallo" << setw(20) << * index_direction;
+//         } else {
+//             list_helper.remove(* index_direction);
+//             list_helper.push_back(* index_direction);
+//             accerts++;
+//             resu << "\n" << setw(20) << direction << setw(20) << "Hit" << setw(20) << * index_direction;
+//         }
+//     }
+//     // resu << endl;
+//     resu.close();
+//     cout << "Proceso completado. Cerrando archivo..." << endl;
+//     cout << "Revise el archivo resu.out" << endl;
+// }
+
 int main() {
 	int block_size ,block_line_size;
 	bool emplazamiento, replace_politic;
@@ -97,11 +144,9 @@ int main() {
 		}
 	}
 	cout << "Leido el archivo traz.in" << endl;
-	// //readTrazIn(direction_list);
 	
 	int number_of_lines = block_size / block_line_size;
-	cout << "\nNumero de lineas calculado" << endl;
-	cout << "Resultado:" << endl;
+	cout << "\nNumero de lineas calculado.\nResultado:" << endl;
     for(list<int> :: iterator line_block = direction_list.begin(); line_block != direction_list.end(); line_block++){
         cout << *line_block << " ";
     }
@@ -109,14 +154,14 @@ int main() {
     if (emplazamiento == 0){
     	cout << "\n\nPreparando proceso de Correspondencia Directa" << endl;
     	if (replace_politic == 0) {
-    		direct_cache_process(direction_list, number_of_lines);
+    		process_LRU(direction_list, number_of_lines);
     	} else {
     		//replaceFIFO()
     	}
     } else {
     	cout << "\n\nPreparando proceso de Asociativa por Conjuntos";
     	if (replace_politic == 0) {
-    		direct_cache_process(direction_list, number_of_lines);
+    		process_LRU(direction_list, number_of_lines);
     	} else {
     		//replaceFIFO()
     	}
